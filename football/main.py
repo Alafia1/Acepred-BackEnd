b219ceb59586684ef706129043b0696a11e15f43
Ismaila Muhammed <mdmuhab@gmail.com>
2023-05-23 13:05:32 +0100

    adding query parameters for matches

--- football/main.py
+++ football/main.py
@@ -1,4 +1,6 @@
+from datetime import date, datetime, timedelta
 from fastapi import Depends, FastAPI, HTTPException, status
+from sqlalchemy import DATE, cast, func
 from sqlalchemy.orm import Session, selectinload
 from typing import List
 import psycopg2
@@ -10,29 +12,112 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-user = {"id": 1, "user": "Wanted"}
-
-data = [{"id": 1, "name": "Ismaila", "user": user}, {"id": 2, "name": "Muhammed", "user": user}]
-
-class User(BaseModel):
-    id: int
-    user: str
-
-class Data(BaseModel):
-    id: int
-    name: str
-    user: User
 
 @app.get("/")
 def root():
     return {"message": "Hello AcePred"}
 
 @app.get("/matches", response_model=List[schemas.Match])
-def get_matches(db: Session = Depends(get_db)):
+def get_matches_on_date(date: date | None = None, league: int | None = None, team: int | None = None, db: Session = Depends(get_db)):
+    if league and team and date:
+        target_datetime = datetime.combine(date, datetime.min.time())
+        next_datetime = target_datetime + timedelta(days=1)
+        matches = db.query(models.Match).options(
+        selectinload(models.Match.league),
+        selectinload(models.Match.home_team),
+        selectinload(models.Match.away_team)
+        ).filter(
+        models.Match.league_id == league, 
+        (models.Match.home_team_id == team) | (models.Match.away_team_id == team),
+        models.Match.datetime >= target_datetime,
+        models.Match.datetime < next_datetime
+        ).all()
+        return matches
+    
+    elif league and team:
+        matches = db.query(models.Match).options(
+            selectinload(models.Match.league),
+            selectinload(models.Match.home_team),
+            selectinload(models.Match.away_team)
+            ).filter(models.Match.league_id == league, (models.Match.home_team_id == team) | (models.Match.away_team_id == team)).all()
+        return matches
+    
+    elif league  and date:
+        target_datetime = datetime.combine(date, datetime.min.time())
+        next_datetime = target_datetime + timedelta(days=1)
+        matches = db.query(models.Match).options(
+        selectinload(models.Match.league),
+        selectinload(models.Match.home_team),
+        selectinload(models.Match.away_team)
+        ).filter(
+        models.Match.league_id == league,
+        models.Match.datetime >= target_datetime,
+        models.Match.datetime < next_datetime
+        ).all()
+        return matches
+    
+    elif team and date:
+        target_datetime = datetime.combine(date, datetime.min.time())
+        next_datetime = target_datetime + timedelta(days=1)
+        matches = db.query(models.Match).options(
+        selectinload(models.Match.league),
+        selectinload(models.Match.home_team),
+        selectinload(models.Match.away_team)
+        ).filter( 
+        (models.Match.home_team_id == team) | (models.Match.away_team_id == team),
+        models.Match.datetime >= target_datetime,
+        models.Match.datetime < next_datetime
+        ).all()
+        return matches
+    
+    elif league:
+        matches = db.query(models.Match).options(
+            selectinload(models.Match.league),
+            selectinload(models.Match.home_team),
+            selectinload(models.Match.away_team)
+            ).filter(models.Match.league_id == league).all()
+        return matches
+    
+    elif team:
+        matches = db.query(models.Match).options(
+            selectinload(models.Match.league),
+            selectinload(models.Match.home_team),
+            selectinload(models.Match.away_team)
+            ).filter(
+            (models.Match.home_team_id == team) | (models.Match.away_team_id == team)
+            ).all()
+        return matches
+    elif date:
+        target_datetime = datetime.combine(date, datetime.min.time())
+        next_datetime = target_datetime + timedelta(days=1)
+        matches = db.query(models.Match).options(
+            selectinload(models.Match.league),
+            selectinload(models.Match.home_team),
+            selectinload(models.Match.away_team)
+        ).filter(
+        models.Match.datetime >= target_datetime,
+        models.Match.datetime < next_datetime
+        ).all()
+        return matches
+
     matches = db.query(models.Match).options(
         selectinload(models.Match.league),
         selectinload(models.Match.home_team),
         selectinload(models.Match.away_team)
+        ).all()
+    return matches
+
+@app.get("/date", response_model=List[schemas.Match])
+def get_matches(date: date, db: Session = Depends(get_db)):
+    target_datetime = datetime.combine(date, datetime.min.time())
+    next_datetime = target_datetime + timedelta(days=1)
+    matches = db.query(models.Match).options(
+        selectinload(models.Match.league),
+        selectinload(models.Match.home_team),
+        selectinload(models.Match.away_team)
+        ).filter(
+        models.Match.datetime >= target_datetime,
+        models.Match.datetime < next_datetime
         ).all()
     return matches
 
@@ -60,7 +145,3 @@ def get_leagues(db: Session = Depends(get_db)):
     leagues = db.query(models.League).all()
     return leagues
 
-@app.get("/data", response_model=List[Data])
-def get_data(db: Session = Depends(get_db)):
-    
-    return data
